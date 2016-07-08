95b33f6240bfa2a75ca799e019d896e1f0c5a9cc
Brienyll <brienyll29@gmail.com>
2016-07-07 21:05:34 -0700

    add code-player

---
+++ script.js
@@ -0,0 +1,31 @@
+
+  var windowHeight=$(window).height();
+  var menuBarHeight=$(menuBar).height();
+
+  var codeContainerHeight=windowHeight-menuBarHeight;
+  $(".codeContainer").height(codeContainerHeight+"px");
+
+  $(".toggle").click(function() {
+
+    $(this).toggleClass("selected");
+
+    var activeDiv=$(this).html();
+
+    $("#"+activeDiv+"Container").toggle();
+
+    var showingDivs = $(".codeContainer").filter(function() {
+
+      return($(this).css("display") != "none");
+    }).length;
+
+    var width=100/showingDivs;
+
+    $(".codeContainer").css ("width",width+"%");
+
+  });
+
+  $("#runButton").click(function() {
+    $("iframe").contents().find("html").html('<style>'+$("#cssCode").val()+'</style>'+$("#htmlCode").val());
+  document.getElemenentById("resultFrame").contentWindow.eval($("#jsCode").val());
+
+  });
